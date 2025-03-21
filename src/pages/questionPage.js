dce0cad3e76a5aa84f481e8bda6529271caa9e15
oleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-03-21 15:53:09 +0100

    resultat and resultat page added

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -10,9 +10,15 @@ import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 import { initResultatPage  } from '../pages/resultatPage.js';
 
+
+let correctAnswerTotal = 0;
+let skipTotal = 0;
+
+
 export const initQuestionPage = (userName) => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
+
   const el = document.createElement('h2');
   // el.style.color = "gray"
   el.textContent = `Player: ${userName}`;
@@ -40,19 +46,37 @@ export const initQuestionPage = (userName) => {
   answersListElements.forEach((answerElement) => {
     const checkAnswer = () => {
       answersListElements.forEach((answerElement) => {
-        answerElement.classList.remove('correct-answer');
-        answerElement.classList.remove('wrong-answer');
+        answerElement.classList.remove('correct-answer', 'wrong-answer');
       });
 
       const { key: userChoice } = answerElement.dataset;
       currentQuestion.selected = userChoice;
-      if (currentQuestion.selected === currentQuestion.correct) {
-        answerElement.classList.add('correct-answer');
-      } else {
+      
+      if (currentQuestion.selected !== currentQuestion.correct) {
         answerElement.classList.add('wrong-answer');
+      }
+
+      answersListElements.forEach((el) => {
+        const { key } = el.dataset;
+        if (key === currentQuestion.correct) {
+          el.classList.add('correct-answer');
+          correctAnswerTotal++;
+        }
+      });
+
+
+
+
+        const hint = document.createElement('a');
+        hint.classList.add('hint');
+        hint.textContent = `Hint: ${currentQuestion.links[0].text}`;
+        hint.href = currentQuestion.links[0].href;
+        document.querySelector('body').appendChild(hint);
       }
     };
     answerElement.addEventListener('click', checkAnswer);
+
+
   });
 
   document
@@ -61,23 +85,45 @@ export const initQuestionPage = (userName) => {
 
   document
     .getElementById(SKIP_QUESTION_BUTTON_ID)
-    .addEventListener('click', () => nextQuestion(userName));
+    .addEventListener('click', () => 
+      {
+        skipTotal++;
+        nextQuestion(userName)
+      });
 
-    //  TODO
-    //  check if the question is last
-    //  showing the result button after the question is answered.
-    document
-    .getElementById(RESULTAT_BUTTON_ID)
-    .addEventListener('click', () => resultat(userName));
 
-  };
+      const resultButton = document.getElementById(RESULTAT_BUTTON_ID);
+      resultButton.style.display = "none"; 
+      if (quizData.currentQuestionIndex === quizData.questions.length - 1) {
+        resultButton.style.display = "block";   
+        resultButton.addEventListener('click', () => {
+              
+const ex = document.getElementById(RESULTAT_BUTTON_ID);
+ex.style.display="none";
+          resultat(userName, correctAnswerTotal, skipTotal);
+
+        });
+      }
+      
+};
 
 const nextQuestion = (userName) => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
+  const hints = document.querySelectorAll('.hint');
+  if (hints) {
+    hints.forEach((hint) => hint.remove());
+  }
+
   initQuestionPage(userName);
+  if (quizData.currentQuestionIndex===9) {
+    const nextQuestionBytton = document.getElementById(NEXT_QUESTION_BUTTON_ID);
+    nextQuestionBytton.style.display="none";
+  }
+
 };
 
-const resultat = (userName) => {  
-  initResultatPage(userName);     
+const resultat = (userName, correctAnswerTotal, skipTotal) => {  
+  
+  initResultatPage(userName, correctAnswerTotal, skipTotal);     
 };
