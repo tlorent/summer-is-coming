2031f6178280661d43279588090a6fa6009d6502
Salma Issa <salmaissa196@gmail.com>
2025-03-24 23:28:07 +0100

    Implement quiz tracker

--- src/constants.js
+++ src/constants.js
@@ -13,3 +13,4 @@ export const SKIP_QUESTION_BUTTON_ID = 'skip-question-button';
 export const WELCOME_BUTTON_ID = 'welcome-quiz-button';
 export const RESULTAT_BUTTON_ID = 'resultat-quiz-button';
 export const ANSWERS_BUTTON_ID = 'answers-result-button';
+export const QUIZ_TRACKER_SECTION = 'quiz-tracker-section';
